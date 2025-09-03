eef1316ba0f83537612007a150dce4bb7703b694
Krzysztof Gębka <krzysztof.gebka2001@gmail.com>
2025-09-03 11:29:42 +0200

    feat: update Header component styling and image size

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,9 @@ import Image from "next/image"
 
 export default function Header() {
     return (
-        <header className="h-20 px-8 flex flex-row justify-between">
-            {/* this image size need to change if i changed height of header */}
-            <Image src={"/logo.svg"} alt="Moje logo" width={120} height={80}/>
+        <header className="h-20 px-8 flex flex-row justify-between shadow-lg">
+            {/* this image size need to change if i changed height of header 36:11*/}
+            <Image src={"/logo.svg"} alt="Moje logo" width={48*36/11} height={48}/>
             <div>
                 others links
             </div>
