e27d7a2d0b32d676b958e4935ffd31127b350e23
Krzysztof Gębka <krzysztof.gebka2001@gmail.com>
2025-10-14 18:25:01 +0200

    center content of hero and little upgrades

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,16 +2,12 @@ import Image from "next/image"
 
 export default function Hero() {
     return (
-        <section>
+        <section className="text-center">
             <div>
-                <div>
-                    <h1>{"Hi, I'm Krzysztof"}</h1>
-                    <p>A developer combining programming skills with an infrastructure perspective</p>
-                </div>
-                <div>
-                    <Image src={"/avatar.png"} alt="avatar icon" width={300} height={300}/>
-                </div>
+                <Image src={"/avatar.png"} alt="avatar icon" width={150} height={150}/>
             </div>
+            <h1>{"Hi, I'm Krzysztof"}</h1>
+            <p>A developer combining programming skills with an infrastructure perspective</p>
         </section>
     )
 }
