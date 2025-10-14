3f86673f6e36c8d145b2eece08ed1a17bcea27a2
Krzysztof Gębka <krzysztof.gebka2001@gmail.com>
2025-10-14 18:24:15 +0200

    comment header and about me

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,10 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <Header/>
-        <Hero/>
-        <AboutMe/>
-      </div>
+    <div className="container">
+      {/* <Header/> */}
+      <Hero/>
+      {/* <AboutMe/> */}
     </div>
   )
 }
